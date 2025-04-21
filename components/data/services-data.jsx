f0f9d6154ac8572f55f798e245fe72096608fbc3
Archange16 <asathoud16@gmail.com>
2025-04-22 01:17:34 +0200

    deploiement

--- components/data/services-data.jsx
+++ components/data/services-data.jsx
@@ -1,39 +1,47 @@
+import services1 from "../../public/assets/img/service/service-details.png";
+
 const servicesData = [
     {
         id: 'site-web',
         icon: <i className="flaticon-website"></i>,
         title: 'Création de sites web',
         description: 'Sites vitrines, e-commerce et portails personnalisés pour mettre en valeur votre activité.',
+        image: services1
     },
     {
         id: 'app-mobile',
         icon: <i className="flaticon-app"></i>,
         title: 'Développement d’applications mobiles',
         description: 'Applications Android/iOS natives ou hybrides avec interfaces intuitives et gestion de contenu.',
+        image: services1
     },
     {
         id: 'design-graphique',
         icon: <i className="flaticon-design"></i>,
         title: 'Design graphique & UX/UI',
         description: 'Création de logos, chartes graphiques, supports de communication et maquettes UI/UX.',
+        image: services1
     },
     {
         id: 'marketing-digital',
         icon: <i className="flaticon-marketing"></i>,
         title: 'Marketing digital & réseaux sociaux',
         description: 'Création de contenu, gestion des réseaux, campagnes publicitaires et stratégie digitale.',
+        image: services1
     },
     {
         id: 'integration-ia',
         icon: <i className="flaticon-artificial-intelligence"></i>,
         title: 'Intégration d’agents IA',
         description: 'Ajout d’agents IA et APIs intelligentes dans vos projets : santé, sport, resto, transport, etc.',
+        image: services1
     },
     {
         id: 'developpement-api',
         icon: <i className="flaticon-code"></i>,
         title: 'Développement d’APIs',
         description: 'Intégration d’APIs Node.js, C#, ou Spring Boot pour connecter et enrichir vos services.',
+        image: services1
     },
 ];
 
--- components/pages/homes/home-2/services.jsx
+++ components/pages/homes/home-2/services.jsx
@@ -16,22 +16,23 @@ const ServicesTwo = () => {
                         </div>
                     </div>
                     <div className="row gy-4 justify-content-center">
-                        {servicesData?.map((data, id) => (
-                            <div className="col-xl-4 col-lg-4 col-md-6" key={id}>
-                                <div className="services__two-single-service">
-                                    <div className="services__two-single-service-icon">
-                                        {data.icon}
-                                    </div>
-                                    <div className="services__two-single-service-content">
-                                        <h4>{data.title}</h4>
-                                        <p>{data.description}</p>
-                                        <Link href={`/services/${data.id}`} className="btn-three">
-                                            En savoir plus <i className="fas fa-chevron-right"></i>
-                                        </Link>
+                        {servicesData?.slice(0, 8).map((data, id) => {
+							const words = data.title.split(' ');
+							const firstAndSecondWord = words.slice(0, 5).join(' ');
+                            return (
+                                <div className="col-xl-4 col-lg-4 col-md-6" key={id}>
+                                    <div className="services__five-single-service">
+                                         <img src={data.image.src} alt="image" />
+                                        <div className="services__five-single-service-content">
+                                        
+                                            <h4>{firstAndSecondWord}</h4>
+                                            <p>{data.description}</p>
+                                            <Link href={`/services/${data.id}`} className="btn-three">En savoir plus<i className="fas fa-chevron-right"></i></Link>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            );
+                        })}
                     </div>
                 </div>
             </div>
