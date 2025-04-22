32024840358a6885219bcd586bd45e23a94e9089
Archange16 <asathoud16@gmail.com>
2025-04-22 13:08:25 +0200

    les modifications

--- components/pages/services/service/services.jsx
+++ components/pages/services/service/services.jsx
@@ -1,3 +1,4 @@
+import servicesData from '@/components/data/services-data';
 import Link from 'next/link';
 
 const ServicesMain = () => {
@@ -5,20 +6,24 @@ const ServicesMain = () => {
         <>
 	    <div className="services__two section-padding">
                 <div className="container">
-                    <div className="row gy-4">
-                        {servicesData?.map((data, id) => (
-                            <div className="col-xl-4 col-lg-4 col-md-6" key={id}>
-                                <div className="services__two-single-service">
-                                    <div className="services__two-single-service-icon">
-                                        {data.icon}
-                                    </div>
-                                    <div className="services__two-single-service-content">
-                                        <h4>{data.title}</h4>
-                                        <Link href={`/services/${data.id}`} className="btn-three">Read More<i className="fas fa-chevron-right"></i></Link>
+                <div className="row gy-4 justify-content-center">
+                        {servicesData?.slice(0, 8).map((data, id) => {
+							const words = data.title.split(' ');
+							const firstAndSecondWord = words.slice(0, 5).join(' ');
+                            return (
+                                <div className="col-xl-4 col-lg-4 col-md-6" key={id}>
+                                    <div className="services__five-single-service">
+                                         <img src={data.image.src} alt="image" />
+                                        <div className="services__five-single-service-content">
+                                        
+                                            <h4>{firstAndSecondWord}</h4>
+                                            <p>{data.description}</p>
+                                            <Link href={`/services/${data.id}`} className="btn-three">En savoir plus<i className="fas fa-chevron-right"></i></Link>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            );
+                        })}
                     </div>
                 </div>
             </div>
